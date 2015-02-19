486f51f8ebca919379dba1e3193ac8c36e5992d2
Leonardo Marinho <leonardomarinho.nf@gmail.com>
2015-02-19 00:05:54 -0200

    Início do projeto do Social Trainee. Nesta vesão montei a splash screen, sobre e instruções.

---
+++ www/js/index.js
@@ -0,0 +1,31 @@
+ function vibrar()
+ {
+	navigator.notification.vibrate(1000);
+  }
+  
+ 
+  function sair()
+  { 
+	  navigator.app.exitApp();
+  }
+  
+  
+  function chamahome()
+  {	  
+ 	  setInterval(function(){window.open('home.html')}, 3000);  
+	  
+  }
+
+  function splash()
+  {
+
+    navigator.splashscreen.show();
+  }
+  
+  
+    function chamatela(tela)
+    {	  
+ 		window.open(tela);  
+	  
+  }
+  
